cfec1e64617b279b47c5404cf56bb0edc1e14b37
Drew Mercer <japanexpress@gmail.com>
2018-02-10 17:02:46 -0500

    Adding a create song create component

---
+++ client/components/SongCreate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Create a New Song</h3>
+                <form>
+                </form>
+            </div>
+        );
+    }
+}
+
+// write a mutation here
+
+export default SongCreate;
