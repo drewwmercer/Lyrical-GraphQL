fd107891ea19231d831780ec349d4cddc441aa37
Drew Mercer <japanexpress@gmail.com>
2018-02-10 13:19:47 -0500

    Avoided error and getting data on screen

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,8 +3,17 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return <li>{song.title}</li>;
+    });
+  }
+
   render() {
-    return <div>SongList in a div</div>;
+    // console.log(this.props);
+    if (this.props.data.loading) { return <div>Loading...</div>; }
+
+    return <div>{this.renderSongs()}</div>;
   }
 }
 
