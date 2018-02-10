784614e8b2e2a2507ade8d091279fde359a666f9
Drew Mercer <japanexpress@gmail.com>
2018-02-10 13:00:23 -0500

    Including query in component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 
 class SongList extends Component {
   render() {
     return <div>SongList in a div</div>;
   }
 }
+
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
 
 export default SongList;
