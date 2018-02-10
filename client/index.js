bea81bab46cb6b977c300b49947eeff2eb599830
Drew Mercer <japanexpress@gmail.com>
2018-02-10 12:35:22 -0500

    Importing apollot

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 const Root = () => {
   return <div>Lyrical</div>
