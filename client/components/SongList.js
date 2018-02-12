10a180105304bb8f15d69319e41e3d696c0f60da
Drew Mercer <japanexpress@gmail.com>
2018-02-11 22:23:00 -0500

    Adding add button

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
@@ -15,7 +16,16 @@ class SongList extends Component {
       return <div>Loading...</div>;
     }
 
-    return <ul className="collection">{this.renderSongs()}</ul>;
+    return (
+      <div>
+        <ul className="collection">
+        {this.renderSongs()}
+        </ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
   }
 }
 
