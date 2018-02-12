f92fa1844d1bd512b50925901fe7758bb2208bf7
Drew Mercer <japanexpress@gmail.com>
2018-02-11 22:25:17 -0500

    Adding back navigation link

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link } from 'react-router';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -20,18 +21,16 @@ class SongCreate extends Component {
   }
 
   render() {
-    return (
-      <div>
+    return <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
-          <input
-            onChange={event => this.setState({ title: event.target.value })}
-            value={this.state.title}
-          />
+          <input onChange={event => this.setState({
+                title: event.target.value
+              })} value={this.state.title} />
         </form>
-      </div>
-    );
+      </div>;
   }
 }
 
