38f1035603705cb8a8f134de79fab34936126e3b
Drew Mercer <japanexpress@gmail.com>
2018-02-11 22:15:23 -0500

    Mutation is now being called via the frontend

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -3,46 +3,45 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class SongCreate extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = { title: '' };
-    }
-    
-    onSubmit(event) {
-        event.preventDefault();
+  constructor(props) {
+    super(props);
 
-        // console.log(this.props);
-        this.props.mutate({
-            variables: {
-                title: this.state.title
-            }
-        });
-    }
+    this.state = { title: '' };
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>Create a New Song</h3>
-                <form onSubmit={this.onSubmit.bind(this)}>
-                    <label>Song Title:</label>
-                    <input 
-                        onChange={event => this.setState({ title: event.target.value })}
-                        value={this.state.title}
-                    />
-                </form>
-            </div>
-        );
-    }
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create a New Song</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input
+            onChange={event => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 // writing the mutation here
 const mutation = gql`
-    mutation AddSong($title: String){
-        addSong(title: $title) {
-            title
-        }
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
     }
+  }
 `;
 
 export default graphql(mutation)(SongCreate);
